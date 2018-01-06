0765f367b92c4b515c3794eb83159c707e959a0b
belloyang <yangbing1113@gmail.com>
2018-01-06 01:42:54 -0400

    First commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { ExampleAppPage } from './app.po';
 
-describe('example-app App', function() {
+describe('Bings App', function() {
   let page: ExampleAppPage;
 
   beforeEach(() => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,10 +16,10 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './example-app/test.ts', watched: false }
+      { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
-      './example-app/test.ts': ['@angular/cli']
+      './src/test.ts': ['@angular/cli']
     },
     mime: {
       'text/x-typescript': ['ts','tsx']
